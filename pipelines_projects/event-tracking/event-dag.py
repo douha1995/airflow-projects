c75afb3391b12abf6c622e6064a327685131cda3
Abdelrahman Mamdouh <abdodouha1995@gmail.com>
2025-02-19 22:11:00 +0200

    load data incrementally and templating task

--- pipelines_projects/event-tracking/event-dag.py
+++ pipelines_projects/event-tracking/event-dag.py
@@ -17,27 +17,32 @@ dag = DAG(
 fetch_events = BashOperator(
     task_id="fetch_events",
     bash_command=(
-        "mkdir -p /data && "
-        "curl -o /data/events.json "
+        "mkdir -p /data/events && "
+        "curl -o /data/events/events{{ds}}.json "
         "https://localhost:5000/events" 
+        "start_date={{ds}}&"
+        "end_date"={{next_ds}}
     ), 
     dag=dag,
 )
 
-def _calculate_stats(input_path, output_path):
+def _calculate_stats(**context):
     """Calculates event statistics."""
+    input_path = context["templates_dict"]["input_path"]
+    output_path = context["templates_dict"]["output_path"]
+    
+    Path(output_path).parent.mkdir(exist_ok=True)
     events = pd.read_json(input_path) 
     stats = events.groupby(["date", "user"]).size().reset_index()
-    Path(output_path).parent.mkdir(exist_ok=True) 
     stats.to_csv(output_path, index=False) 
  
  
 calculate_stats = PythonOperator(
     task_id="calculate_stats",
     python_callable=_calculate_stats,
-    op_kwargs={
-    "input_path": "/data/events.json",
-    "output_path": "/data/stats.csv",
+    templates_dict={
+        "input_path": "/data/events/{{ds}}",
+        "output_path": "/data/stats/{{ds}}.csv",
     },
     dag=dag,
 )
